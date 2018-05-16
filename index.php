adf84106e350c74e2b1260696f33b690bcc21ed4
CRU85 <crubs.one@gmail.com>
2018-05-16 10:42:41 +0200

    Modification de la description et de l'extension

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Croque-monsieur</title>
-    <meta name="description" content="Générateur de recettes de croque-monsieur">
+    <meta name="description" content="Un délicieux générateur de recettes de croque-monsieur">
     <meta name="robots" content="index, follow">
     <link rel="shortcut icon" href="img/icon.ico">
     <link rel="stylesheet" type="text/css" href="css/main.css">
   </head>
   <body>
     <h1>Croque-monsieur</h1>
-    <h2>Générateur de recettes de croque-monsieur</h2>
+    <h2>Un délicieux générateur de recettes de croque-monsieur</h2>
   </body>
 </html>
